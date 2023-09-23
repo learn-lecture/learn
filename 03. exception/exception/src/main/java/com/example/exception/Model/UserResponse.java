9e74406b1a8026dd66c438eb09500d230d60acb9
jihwankim128 <jihwan1535@nate.com>
2023-09-24 02:50:33 +0900

    User응 DTO

---
+++ 03. exception/exception/src/main/java/com/example/exception/Model/UserResponse.java
@@ -0,0 +1,16 @@
+package com.example.exception.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private String id;
+    private String name;
+    private Integer age;
+}
