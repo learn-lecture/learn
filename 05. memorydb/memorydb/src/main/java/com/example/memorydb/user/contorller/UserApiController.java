aaca5fba2c9b07ad4c46dd555ab7ba6d78962557
jihwankim128 <jihwan1535@nate.com>
2023-09-28 02:36:01 +0900

    Using a service to control

---
+++ 05. memorydb/memorydb/src/main/java/com/example/memorydb/user/contorller/UserApiController.java
@@ -0,0 +1,58 @@
+package com.example.memorydb.user.contorller;
+
+import com.example.memorydb.user.model.UserEntity;
+import com.example.memorydb.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+// Business Logic
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PutMapping("")
+    public UserEntity create(@RequestBody UserEntity userEntity) {
+        return userService.save(userEntity);
+    }
+
+    @GetMapping("/all")
+    public List<UserEntity> findAll(
+        @RequestParam(name = "score", defaultValue = "0") int score
+    ) {
+        if(score == 0) return userService.findAll();
+        return userService.highScore(score);
+    }
+
+    @GetMapping("/id/{id}")
+    public Optional<UserEntity> findById(
+        @PathVariable Long id
+    ) {
+        return userService.findById(id);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(
+        @RequestParam Long id
+    ) {
+        userService.delete(id);
+    }
+
+    @GetMapping("/highscore")
+    public List<UserEntity> highSocre(
+        @RequestParam int score
+    ){
+        return userService.highScore(score);
+    }
+}
+/*
+Business Logic을 처리하는 부분 = Service Logic
+Service Logic은 Repasitory DataBase와 붙어있는 영역
+Controller -> Service로 Request
+Service -> DataBase(Repasitory)를 통해서 Data를 처리 후 Response
+*/
