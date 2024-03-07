f7082133ffa503e6fd998fe5ee10f5483d46e993
jihwan <jihwan1535@nate.com>
2024-03-07 17:58:08 +0900

    feat : Create a Servlet on a Servlet Container

--- src/main/java/com/toby/spring/Application.java
+++ src/main/java/com/toby/spring/Application.java
@@ -1,5 +1,7 @@
 package com.toby.spring;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
@@ -9,13 +11,31 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class Application {
 
 	public static void main(String[] args) {
 		// Tomcat 외 다른 서버도 구현할 수 있도록 interface로 명시되어 있음.
 		final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		final WebServer webServer = serverFactory.getWebServer();
+		final WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws
+					ServletException,
+					IOException {
+					// 웹 응답의 3가지 요소
+					// 1. status line에서 status code
+					// 2. create header, especially contentType header
+					// 3. body
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello");
+		});
 		webServer.start();
 		// ----- Success WebServer Start -----
 	}
