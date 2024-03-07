78929ee3a18cdb5e1960c883c4d71c5f41328c5a
jihwan <jihwan1535@nate.com>
2024-03-07 17:47:41 +0900

    feat : Servlet Container on a webServer

--- src/main/java/com/toby/spring/Application.java
+++ src/main/java/com/toby/spring/Application.java
@@ -2,12 +2,22 @@ package com.toby.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		// Tomcat 외 다른 서버도 구현할 수 있도록 interface로 명시되어 있음.
+		final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		final WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
+		// ----- Success WebServer Start -----
 	}
 
 }
