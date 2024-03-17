a3600debb8a58a5a68d8bda1a69e82e223816702
jihwan <jihwan1535@nate.com>
2024-03-17 23:39:43 +0900

    refactor : Change Servlet -> dispatcher Servlet
    dispatcher servlet을 통해 처리하므로 코드로 매핑하던 내용을annotation으로 매핑한다.
    
    method 레벨에서 찾게 된다면 Handler가 수 많은 정보에서 매핑 정보를 찾아야하므로 Class 레벨에도 매핑 정보를 추가하는게 좋다.
    
    Dispatcher의 MVC 동작방식 때문에 View의 정보를 리턴해야하므로 Response 형태로 반환해주어야 한다.
    
    Spring 3.x부터는 클래스 및 Method 레벨 수준으로 매핑 할 수 없다. @Controller 어노테이션을 꼭 추가 해야함.

--- src/main/java/com/toby/spring/Application.java
+++ src/main/java/com/toby/spring/Application.java
@@ -12,6 +12,9 @@ import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import com.toby.spring.contoller.HelloController;
 import com.toby.spring.service.SimpleHelloService;
@@ -26,8 +29,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		// Spring Container
-		// resource, evnet, ...
-		final GenericApplicationContext applicationContext = new GenericApplicationContext();
+		// resource, event, ...
+		final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		// bean 등록 후 초기화하기
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
@@ -36,30 +39,10 @@ public class Application {
 		// Tomcat 외 다른 서버도 구현할 수 있도록 interface로 명시되어 있음.
 		final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		final WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws
-					ServletException,
-					IOException {
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						final String name = req.getParameter("name");
-
-						// 빈 불러오기
-						final HelloController helloController = applicationContext.getBean(HelloController.class);
-						// hello 호출에 대해 맵핑과 바인딩
-						final String ret = helloController.hello(name);
-
-						// 웹 응답의 3가지 요소
-						// 1. status line에서 status code
-						// 2. create header, especially contentType header
-						// 3. body
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, "text/plain");
-						resp.getWriter().println(ret);
-					} else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-				}
-			}).addMapping("/*");
+			servletContext.addServlet(
+				"frontcontroller",
+				new DispatcherServlet(applicationContext)
+			).addMapping("/*");
 		});
 		webServer.start();
 		// ----- Success WebServer Start -----
--- src/main/java/com/toby/spring/contoller/HelloController.java
+++ src/main/java/com/toby/spring/contoller/HelloController.java
@@ -2,9 +2,15 @@ package com.toby.spring.contoller;
 
 import java.util.Objects;
 
-import com.toby.spring.service.HelloService;
-import com.toby.spring.service.SimpleHelloService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.toby.spring.service.HelloService;
+
+@Controller
+@RequestMapping
 public class HelloController {
 
 	private final HelloService helloService;
@@ -13,6 +19,8 @@ public class HelloController {
 		this.helloService = helloService;
 	}
 
+	@GetMapping("/hello")
+	@ResponseBody
 	public String hello(final String name) {
 		return helloService.sayHello(Objects.requireNonNull(name));
 	}
