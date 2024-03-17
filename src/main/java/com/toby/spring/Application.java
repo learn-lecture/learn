4fd9d93b4e57eead3552c0bd127286ee05ba2722
jihwan <jihwan1535@nate.com>
2024-03-17 17:33:03 +0900

    refactor : registered bean on a spring container
    - applicationContext를 활용해서 스프링 컨테이너 생성
    - HelloController Class를 bean으로 등록
    - Spring Container에 등록된 bean 중 HelloController를 가져와서 Hello
      호출에 대한 처리

--- src/main/java/com/toby/spring/Application.java
+++ src/main/java/com/toby/spring/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -23,10 +24,16 @@ import jakarta.servlet.http.HttpServletResponse;
 public class Application {
 
 	public static void main(String[] args) {
+		// Spring Container
+		// resource, evnet, ...
+		final GenericApplicationContext applicationContext = new GenericApplicationContext();
+		// bean 등록 후 초기화하기
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.refresh();
+
 		// Tomcat 외 다른 서버도 구현할 수 있도록 interface로 명시되어 있음.
 		final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		final WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			final HelloController helloController = new HelloController();
 			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws
@@ -35,6 +42,8 @@ public class Application {
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						final String name = req.getParameter("name");
 
+						// 빈 불러오기
+						final HelloController helloController = applicationContext.getBean(HelloController.class);
 						// hello 호출에 대해 맵핑과 바인딩
 						final String ret = helloController.hello(name);
 
@@ -42,11 +51,8 @@ public class Application {
 						// 1. status line에서 status code
 						// 2. create header, especially contentType header
 						// 3. body
-						resp.setStatus(HttpStatus.OK.value());
 						resp.setHeader(HttpHeaders.CONTENT_TYPE, "text/plain");
 						resp.getWriter().println(ret);
-					} else if (req.getRequestURI().equals("/test")) {
-						// default status = 200
 					} else {
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
 					}
