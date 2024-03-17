cb359ea5aa0f5b2913b7697bea62a13e46c01a03
jihwan <jihwan1535@nate.com>
2024-03-17 17:52:11 +0900

    feat : HelloController의 의존 서비스 추가

--- src/main/java/com/toby/spring/contoller/HelloController.java
+++ src/main/java/com/toby/spring/contoller/HelloController.java
@@ -1,9 +1,15 @@
 package com.toby.spring.contoller;
 
+import java.util.Objects;
+
+import com.toby.spring.service.SimpleHelloService;
+
 public class HelloController {
 
+	final SimpleHelloService simpleHelloService = new SimpleHelloService();
+
 	public String hello(final String name) {
-		return "Hello " + name;
+		return simpleHelloService.sayHello(Objects.requireNonNull(name));
 	}
 
 }
---
+++ src/main/java/com/toby/spring/service/SimpleHelloService.java
@@ -0,0 +1,9 @@
+package com.toby.spring.service;
+
+public class SimpleHelloService {
+
+	public String sayHello(final String name) {
+		return "Hello " + name;
+	}
+
+}
