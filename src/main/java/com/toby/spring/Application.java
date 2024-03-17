e3c8fe190d86f265b096d803e2cf95741043b02a
jihwan <jihwan1535@nate.com>
2024-03-17 17:04:54 +0900

    feat: front controller mapping and binding

--- src/main/java/com/toby/spring/Application.java
+++ src/main/java/com/toby/spring/Application.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
+import com.toby.spring.contoller.HelloController;
+
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -24,20 +26,25 @@ public class Application {
 		// Tomcat 외 다른 서버도 구현할 수 있도록 interface로 명시되어 있음.
 		final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		final WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			final HelloController helloController = new HelloController();
 			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws
 					ServletException,
 					IOException {
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
+						final String name = req.getParameter("name");
+
+						// hello 호출에 대해 맵핑과 바인딩
+						final String ret = helloController.hello(name);
+
 						// 웹 응답의 3가지 요소
 						// 1. status line에서 status code
 						// 2. create header, especially contentType header
 						// 3. body
 						resp.setStatus(HttpStatus.OK.value());
 						resp.setHeader(HttpHeaders.CONTENT_TYPE, "text/plain");
-						resp.getWriter().println("Hello " + name);
+						resp.getWriter().println(ret);
 					} else if (req.getRequestURI().equals("/test")) {
 						// default status = 200
 					} else {
---
+++ src/main/java/com/toby/spring/contoller/HelloController.java
@@ -0,0 +1,9 @@
+package com.toby.spring.contoller;
+
+public class HelloController {
+
+	public String hello(final String name) {
+		return "Hello " + name;
+	}
+
+}
