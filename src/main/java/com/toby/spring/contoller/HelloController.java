8fe9fe40d4b3dc16debf4bd58bac922bf1531d4f
jihwan <jihwan1535@nate.com>
2024-03-25 10:56:02 +0900

    test : spring bean life cycle test
    Dispatcher Servlet에서 Application Context Aware 인터페이스를 구현하면서 spring bean life cycle에 의해 자동으로 주입되는 것을 테스트

--- src/main/java/com/toby/spring/contoller/HelloController.java
+++ src/main/java/com/toby/spring/contoller/HelloController.java
@@ -2,6 +2,8 @@ package com.toby.spring.contoller;
 
 import java.util.Objects;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +16,16 @@ import com.toby.spring.service.HelloService;
 public class HelloController {
 
 	private final HelloService helloService;
+	private final ApplicationContext applicationContext;
 
-	public HelloController(final HelloService helloService) {
+	public HelloController(
+		final HelloService helloService,
+		final ApplicationContext applicationContext
+	) {
 		this.helloService = helloService;
+		this.applicationContext = applicationContext;
+
+		System.out.println(applicationContext);
 	}
 
 	@GetMapping("/hello")
