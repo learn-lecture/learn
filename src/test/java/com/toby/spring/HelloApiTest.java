9bfb65c17a22db13453bf304a851dc9b35b8eed9
jihwan <jihwan1535@nate.com>
2024-03-29 02:36:18 +0900

    test : Hello API Test
    서버가 구동중 일 때, RestTemplate을 사용해서 통신된 정보를 테스트한다.

---
+++ src/test/java/com/toby/spring/HelloApiTest.java
@@ -0,0 +1,28 @@
+package com.toby.spring;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+	@Test
+	void helloApi() {
+		final TestRestTemplate rest = new TestRestTemplate();
+
+		final ResponseEntity<String> res =
+			rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "param");
+
+		assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+		assertThat(res.getBody()).isEqualTo("Hello param");
+	}
+
+}
