cb43773bcb99f52f1f382a1eaa1510b7ac110dbd
jihwan <jihwan1535@nate.com>
2024-04-13 21:53:48 +0900

    jwt api and test
    setClaims, setExpiration, parserBuilder, SigningKey 등 jwt 0.11.5 와 비교해서 바뀐 부분이 많다.
    
    1. jwt는 기본적으로 암호화 할 암호키에 암호 알고리즘윽 추가한다.
    2. claims 은 토큰에 포함된 정보로 키:쌍 값으로 서버에서 보면 세션 정보, 클라이언트에서 보면 쿠키 정보와 같은 형태이다.
    -> jwt에서는 두 가지 유형의 클레임만 제공되는데 토큰 생성시 만들어지는 클레임 (값)과 클레임 정보에 등록된 클레임 (키)으로 구성되어있다.
    3. 만료시간, 현재시간을 추가한 뒤 생성한다.
    
    테스트
    임의의 클레임을 생성하고 토큰이 제대로 생성되는지 확인했다.
    생성된 토큰을 이용해 토큰 검증 테스트도 완료했다.
    만약 토큰 유효기간이 지났을 때, 예외가 발생하는 것도 확인되었다.
    그 외, 시그니쳐 사인이나 암호, 페이로드에 대한 예외도 확인되었다.

--- jwt/src/test/java/com/study/jwt/JwtApplicationTests.java
+++ jwt/src/test/java/com/study/jwt/JwtApplicationTests.java
@@ -1,13 +1,40 @@
 package com.study.jwt;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.study.jwt.service.JwtService;
 
 @SpringBootTest
 class JwtApplicationTests {
+
+	@Autowired
+	private JwtService jwtService;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void tokenCreate() {
+		final HashMap<String, Object> claims = new HashMap<>();
+		final int expiredAt = 30000;
+
+		claims.put("user_id", 100);
+
+		final String s = jwtService.create(claims, expiredAt);
+
+		System.out.println(s);
+	}
+
+	@Test
+	void tokenValidation() {
+		final String token =
+			"eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMDAsImV4cCI6MTcxMzAxMjQ2MywiaWF0IjoxNzEzMDEyNDMzfQ.-qWKWcT2lFOLZa5LTdWCi36DrIMke-EW2PjPW39HjpA";
+		jwtService.validation(token);
+	}
 }
