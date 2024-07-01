5ee0689ec3a74d3ed7bf244469f5ec1344bab9f6
jihwan <jihwan1535@nate.com>
2024-07-01 16:56:12 +0900

    refactor : divide auto configuration info
    text file 로부터 import 할 정보를 가져옴
    ClassLoader 로부터 import 한 class 정보를 parsing
    parsing 한 class 정보를 bean 으로 등록

--- src/main/java/com/toby/config/MyAutoConfigImportSelector.java
+++ src/main/java/com/toby/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,30 @@
 package com.toby.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+	private final ClassLoader classLoader;
+
+	public MyAutoConfigImportSelector(final ClassLoader classLoader) {
+		this.classLoader = classLoader;
+	}
+
 	@Override
 	public String[] selectImports(final AnnotationMetadata importingClassMetadata) {
-		return new String[] {
-			"com.toby.config.autoconfig.DispatcherServletConfig",
-			"com.toby.config.autoconfig.TomcatWebServerConfig"
-		};
+		//final Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+		//return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
+		final List<String> autoConfigs = new ArrayList<>();
+
+		ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+		return autoConfigs.toArray(new String[0]);
 	}
+
 }
