8e9e2390387b340f1fa5e6a2e5df631d4e59da03
jihwan <jihwan1535@nate.com>
2024-07-02 18:58:40 +0900

    feat: jetty server 구성
    - infra structure bean 으로 jetty server 를 구성함.
    - conditional annotation 을 활용해서 tomcat, jetty 중 일단은 jetty 로 사용되게 활용
    - conditional annotation 을 사용하면 Condition interface 를 구현한 객체가 있어야 함.

---
+++ src/main/java/com/toby/config/autoconfig/JettyWebServerConfig.java
@@ -0,0 +1,29 @@
+package com.toby.config.autoconfig;
+
+import com.toby.config.MyAutoConfiguration;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+@MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class)
+public class JettyWebServerConfig {
+
+	@Bean(name = "jettyWebServetFactory")
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new JettyServletWebServerFactory();
+	}
+
+	static class JettyCondition implements Condition {
+		@Override
+		public boolean matches(final ConditionContext context, final AnnotatedTypeMetadata metadata) {
+			return true;
+		}
+	}
+
+}
--- src/main/java/com/toby/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/toby/config/autoconfig/TomcatWebServerConfig.java
@@ -2,17 +2,25 @@ package com.toby.config.autoconfig;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 import com.toby.config.MyAutoConfiguration;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
 
-	@Bean
+	@Bean(name = "tomcatWebServetFactory")
 	public ServletWebServerFactory servletWebServerFactory() {
 		return new TomcatServletWebServerFactory();
+	}
+
+	static class TomcatCondition implements Condition {
+		@Override
+		public boolean matches(final ConditionContext context, final AnnotatedTypeMetadata metadata) {
+			return false;
+		}
 	}
 
 }
