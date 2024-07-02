80bb1b273dbfd97415447eb9495f5a40f914068b
jihwan <jihwan1535@nate.com>
2024-07-03 00:23:01 +0900

    feat : 사용자 자동구성 정보로 대체
    - ConditionalOnMissingBean Annotation 으로 사용자 측에서 자동구성 정보를 Bean 으로 등록하면 Spring Framework 에 작성된 자동구성 정보를 대체할 수 있다.
    - ConditionalOnMissingBean 은 사용자 측에서 자동구성 정보를 정의한 것이 있는지 먼저 확인하고 없으면 스프링에서 정의한 자동 구성 정보를 사용하도록 한다.
    - ObjectMapper 같은 경우 Spring start jackson 으로 이미 자동구성 정보가 존재하지만 java time8 과 같이 추가적인 정보를 등록하려고하면 ObjectMapper 를 셋팅 후 Bean 으로 등록할 때가 있다. 이것이 되는 이유가 ConditionalOnMissingBean 때문이다.

--- src/main/java/com/toby/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/toby/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,8 @@ package com.toby.config.autoconfig;
 
 import com.toby.config.ConditionalMyOnClass;
 import com.toby.config.MyAutoConfiguration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 public class TomcatWebServerConfig {
 
     @Bean(name = "tomcatWebServetFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
