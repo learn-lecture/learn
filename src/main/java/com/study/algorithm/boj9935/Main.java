a54d7b6dfc9010b071e07ce4d0230b32ea5fecd8
jihwan <jihwan1535@nate.com>
2024-07-06 21:45:54 +0900

    ps: boj 9935
    100만 * 36 = 3600만
    매번 폭발단어 길이만큼 되었을 때, 폭발할 수 있는지 확인한다.
    n-1 번 째에서 폭발했을 경우, n-1번 째 폭발로 인해 n번째 폭발에 영향을 미칠 수 없다.
    그러므로 매번 마지막 단어가 추가될 경우만 폭발하는지 확인하면 된다.

---
+++ src/main/java/com/study/algorithm/boj9935/Main.java
@@ -0,0 +1,51 @@
+package com.study.algorithm.boj9935;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter BW = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder SB = new StringBuilder();
+	static String NO_CHARACTER = "FRULA";
+
+	public static void main(String[] args) throws IOException {
+		String str = BR.readLine();
+		String boomStr = BR.readLine();
+		int boomLength = boomStr.length();
+
+		Stack<Character> st = new Stack<>();
+
+		for (int i = 0; i < str.length(); i++) {
+			st.push(str.charAt(i));
+			if (st.size() >= boomLength) {
+				boolean isEqual = true;
+				for (int j = 0; j < boomLength; j++) {
+					if (st.get(j + (st.size() - boomLength)) != boomStr.charAt(j)) {
+						isEqual = false;
+						break;
+					}
+				}
+				if (isEqual) {
+					for (int j = 0; j < boomLength; j++) {
+						st.pop();
+					}
+				}
+			}
+		}
+
+		if (st.empty()) {
+			SB.append(NO_CHARACTER);
+		} else {
+			for (char ch : st) {
+				SB.append(ch);
+			}
+		}
+
+		BW.write(SB.toString());
+		BW.close();
+		BR.close();
+	}
+
+}
