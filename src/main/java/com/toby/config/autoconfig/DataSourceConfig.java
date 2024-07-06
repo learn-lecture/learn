b7987852666c1d98026107ba13260c7b72af72d5
jihwan <jihwan1535@nate.com>
2024-07-07 07:54:25 +0900

    feat: Hikari Pool DataSource

--- src/main/java/com/toby/config/autoconfig/DataSourceConfig.java
+++ src/main/java/com/toby/config/autoconfig/DataSourceConfig.java
@@ -4,12 +4,15 @@ import java.sql.Driver;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.jdbc.metadata.HikariDataSourcePoolMetadata;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import com.toby.config.ConditionalMyOnClass;
 import com.toby.config.EnableMyConfigurationProperties;
 import com.toby.config.MyAutoConfiguration;
+import com.zaxxer.hikari.HikariDataSource;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
@@ -17,8 +20,23 @@ import com.toby.config.MyAutoConfiguration;
 public class DataSourceConfig {
 
 	@Bean
+	@ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
+	@ConditionalOnMissingBean
+	DataSource hikariDataSource(final MyDatasourceProperties properties) {
+		final HikariDataSource dataSource = new HikariDataSource();
+
+		dataSource.setDriverClassName(properties.getDriverClassName());
+		dataSource.setJdbcUrl(properties.getUrl());
+		dataSource.setUsername(properties.getUsername());
+		dataSource.setPassword(properties.getPassword());
+
+		return dataSource;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
 	DataSource dataSource(final MyDatasourceProperties properties) throws ClassNotFoundException {
-		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		final SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 
 		dataSource.setDriverClass((Class<? extends Driver>)Class.forName(properties.getDriverClassName()));
 		dataSource.setUrl(properties.getUrl());
