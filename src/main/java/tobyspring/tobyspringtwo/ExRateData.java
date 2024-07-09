c1cb026275ebc59609302ff602c57c91b7979c9a
jihwan <jihwan1535@nate.com>
2024-07-10 07:27:16 +0900

    feat: get exchange Rate

---
+++ src/main/java/tobyspring/tobyspringtwo/ExRateData.java
@@ -0,0 +1,10 @@
+package tobyspring.tobyspringtwo;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
+}
--- src/main/java/tobyspring/tobyspringtwo/PaymentService.java
+++ src/main/java/tobyspring/tobyspringtwo/PaymentService.java
@@ -1,7 +1,17 @@
 package tobyspring.tobyspringtwo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PaymentService {
 
@@ -9,9 +19,22 @@ public class PaymentService {
 		final Long orderId,
 		final String currency,
 		final BigDecimal foreignCurrencyAmount
-	) {
+	) throws IOException {
 		// todo 환율가져오기
+		// https://open.er-api.com/v6/latest/USD
+		final URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+		final HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		final BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		final String response = br.lines().collect(Collectors.joining());
+		br.close();
+
+		final ObjectMapper mapper = new ObjectMapper();
+		final ExRateData data = mapper.readValue(response, ExRateData.class);
+		final BigDecimal exRate = data.rates().get("KRW");
+		System.out.println(exRate);
+
 		// todo 금액 계산
+
 		// todo 유효 시간 계산
 		return Payment.builder()
 			.orderId(orderId)
@@ -23,7 +46,7 @@ public class PaymentService {
 			.build();
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		final PaymentService paymentService = new PaymentService();
 		final Payment prepare = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
 		System.out.println(prepare);
