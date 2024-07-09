ee76b8971d5052a001b0e4768e0316bbe323eaab
jihwan <jihwan1535@nate.com>
2024-07-10 06:42:05 +0900

    chore: init

---
+++ src/main/java/tobyspring/tobyspringtwo/TobySpringTwoApplication.java
@@ -0,0 +1,13 @@
+package tobyspring.tobyspringtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobySpringTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tobyspring/tobyspringtwo/TobySpringTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.tobyspringtwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
