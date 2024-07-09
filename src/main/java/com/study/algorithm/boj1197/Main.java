daa591a143a0ad3d7cf6f4e3bfb321488d35d04f
jihwan <jihwan1535@nate.com>
2024-07-09 17:18:24 +0900

    ps: boj 1197

--- src/main/java/com/study/algorithm/boj1197/Main.java
+++ src/main/java/com/study/algorithm/boj1197/Main.java
@@ -3,75 +3,70 @@ package com.study.algorithm.boj1197;
 import java.io.*;
 import java.util.*;
 
-class Edge {
+class Edge implements Comparable<Edge>{
 
-	int v;
-	int w;
+    int v;
+    int w;
 
-	Edge(int v, int w) {
-		this.v = v;
-		this.w = w;
-	}
+    Edge(int v, int w) {
+        this.v = v;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return this.w - o.w;
+    }
 
 }
 
 public class Main {
+    static BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter BW = new BufferedWriter(new OutputStreamWriter(System.out));
+    static List<Edge>[] edges = new List[10001];
+    static boolean[] visited = new boolean[10001];
+    static int N, M;
 
-	static BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter BW = new BufferedWriter(new OutputStreamWriter(System.out));
-	static ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
-	static int dist[] = new int[10001];
-	static int N, M;
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(BR.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int distance = 0;
 
-	public static void main(String[] args) throws IOException {
-		StringTokenizer st = new StringTokenizer(BR.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            edges[i] = new ArrayList<>();
+        }
 
-		for (int i = 0; i <= N; i++) {
-			graph.add(new ArrayList<>());
-		}
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(BR.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            edges[u].add(new Edge(v, w));
+            edges[v].add(new Edge(u, w));
+        }
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(BR.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(1, 0));
 
-			graph.get(u).add(new Edge(v, w));
-			graph.get(v).add(new Edge(u, w));
-		}
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
 
-		bellmanFord();
-		for (int i = 0; i <= N; i++) {
-			System.out.print(dist[i] + " ");
-		}
-		System.out.println();
-		int minDist = Integer.MAX_VALUE;
-		for (int i = 2; i <= N; i++) {
-			Math.min(minDist, dist[i]);
-		}
-		BW.write(String.valueOf(minDist));
-		BW.flush();
-		BW.close();
-		BR.close();
-	}
+            if (visited[edge.v]) continue;
+            visited[edge.v] = true;
+            distance += edge.w;
 
-	private static void bellmanFord() {
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		dist[1] = 0;
+            for (Edge next: edges[edge.v]) {
+                if (!visited[next.v]) {
+                    pq.offer(next);
+                }
+            }
+        }
 
-		for (int i = 1; i < N; i++) {
-			for (int v = 1; v <= N; v++) {
-				if (dist[v] == Integer.MAX_VALUE) continue;
-				for (Edge e: graph.get(v)) {
-					if (dist[e.v] > dist[v] + e.w) {
-						dist[e.v] = dist[v] + e.w;
-					}
-				}
-			}
-		}
-	}
-
+        BW.write(String.valueOf(distance));
+        BW.flush();
+        BW.close();
+        BR.close();
+    }
 
 }
