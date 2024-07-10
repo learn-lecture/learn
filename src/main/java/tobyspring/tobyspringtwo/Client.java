93f1dedf1419a071e0eace3969947b75c8a3864a
jihwan <jihwan1535@nate.com>
2024-07-11 08:08:22 +0900

    refactor: DI On Spring Container
    - Spring Container 를 이해하기 위해 Bean Factory 를 활용한다.
    - Object 를 Spring Bean 으로 만들어 의존성 주입(DI)을 한다.

--- src/main/java/tobyspring/tobyspringtwo/Client.java
+++ src/main/java/tobyspring/tobyspringtwo/Client.java
@@ -3,11 +3,14 @@ package tobyspring.tobyspringtwo;
 import java.io.IOException;
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Client {
 
 	public static void main(String[] args) throws IOException {
-		final ObjectFactory objectFactory = new ObjectFactory();
-		final PaymentService paymentService = objectFactory.paymentService();
+		final BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+		final PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 		final Payment prepare = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
 		System.out.println(prepare);
 	}
