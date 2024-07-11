f332862f81b1dfd4bc7334a9649e12aa3e2d60f8
jihwan <jihwan1535@nate.com>
2024-07-11 14:00:53 +0900

    ps: boj 4991

--- src/main/java/com/study/algorithm/boj4991/Main.java
+++ src/main/java/com/study/algorithm/boj4991/Main.java
@@ -4,17 +4,15 @@ import java.io.*;
 import java.util.*;
 
 class Pair {
-
     int x;
     int y;
     int cnt;
 
-    Pair (int x, int y, int cnt) {
+    Pair(int x, int y, int cnt) {
         this.x = x;
         this.y = y;
         this.cnt = cnt;
     }
-
 }
 
 public class Main {
@@ -23,83 +21,81 @@ public class Main {
     static BufferedWriter BW = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder SB = new StringBuilder();
     static char[][] ROOM = new char[21][21];
-    static boolean[][] visited = new boolean[21][21];
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
-    static int N, M;
-    static Pair SPOT = new Pair(0, 0, 0);
+    static int N, M, DIRTY_COUNT;
+    static Pair START;
+    static int TOTAL_DISTANCE;
 
     public static void main(String[] args) throws IOException {
-        while (input()) {
-            Pair spot = bfs(SPOT);
-            if (Objects.equals(SPOT, spot)) {
-                SB.append(-1);
-                continue;
-            }
-            spot.cnt += SPOT.cnt;
-            SPOT = spot;
 
-            for (int i = 0; i < 9; i++) {
-                spot = bfs(SPOT);
-                if (Objects.equals(SPOT, bfs(SPOT))) {
-                    break;
-                }
-                spot.cnt += SPOT.cnt;
-                SPOT = spot;
+        while (input()) {
+            TOTAL_DISTANCE = Integer.MAX_VALUE;
+            rec(START, 0);
+            DIRTY_COUNT = 0;
+            if (TOTAL_DISTANCE == Integer.MAX_VALUE) {
+                TOTAL_DISTANCE = -1;
             }
-            SB.append(SPOT.cnt).append('\n');
+            SB.append(TOTAL_DISTANCE).append("\n");
         }
+
         BW.write(SB.toString());
         BW.flush();
         BW.close();
         BR.close();
     }
 
-    private static Pair bfs(Pair start) {
-        for (boolean vis[]: visited) {
-            Arrays.fill(vis, false);
+    private static void rec(Pair start, int k) {
+        if (k == DIRTY_COUNT) {
+            TOTAL_DISTANCE = Math.min(TOTAL_DISTANCE, start.cnt);
         }
 
+
+        boolean[][] visited = new boolean[21][21];
         Queue<Pair> q = new LinkedList<>();
         q.offer(start);
+        visited[start.x][start.y] = true;
 
         while (!q.isEmpty()) {
             Pair p = q.poll();
+
+            if(p.cnt >= TOTAL_DISTANCE) return ;
+
             if (ROOM[p.x][p.y] == '*') {
                 ROOM[p.x][p.y] = '.';
-                return p;
+                rec(p, k + 1);
+                ROOM[p.x][p.y] = '*';
             }
+
             for (int i = 0; i < 4; i++) {
                 int nx = p.x + dx[i];
                 int ny = p.y + dy[i];
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny]) continue;
                 if (ROOM[nx][ny] == 'x') continue;
-                if(visited[nx][ny]) continue;
                 visited[nx][ny] = true;
                 q.offer(new Pair(nx, ny, p.cnt + 1));
             }
         }
-
-        return start;
     }
 
     private static boolean input() throws IOException {
         StringTokenizer st = new StringTokenizer(BR.readLine());
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        if (M == 0 && N == 0) {
+        if (N == 0 && M == 0) {
             return false;
         }
+
         for (int i = 0; i < N; i++) {
             String info = BR.readLine();
-            for (int j = 0; j < info.length(); j++) {
-                if (info.charAt(j) == 'o') {
+            for (int j = 0; j < M; j++) {
+                ROOM[i][j] = info.charAt(j);
+                if (ROOM[i][j] == 'o') {
+                    START = new Pair(i, j, 0);
                     ROOM[i][j] = '.';
-                    SPOT.x = i;
-                    SPOT.y = j;
-                    SPOT.cnt = 0;
-                } else {
-                    ROOM[i][j] = info.charAt(j);
+                }
+                if (ROOM[i][j] == '*') {
+                    DIRTY_COUNT++;
                 }
             }
         }
