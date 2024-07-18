ea4e7ed6dd4b3990dfbf4eabe4b42c90df40b6a1
jihwan <jihwan1535@nate.com>
2024-07-18 17:38:27 +0900

    refactor: transaction service abstract
    - spring 에서는 transaction 과 같이 다른 platform 으로 부터 사용하는 기술들을 추상화해두었다.
    - 다른 platform 의 기술이 추상화된 메소드와 동일한 메소드명을 가졌을리 없다. 그래서 spring 은 자동 구성된 정보에 따라 다른 platform 의 기술을 사용하기 위해 adaptor pattern 을 사용해서 자동 구성 정보에 상기된 내용으로 변환해준다.
    - PlatformTransactionManager, 트랜잭션의 경우 어댑터를 사용하는 인터페이스이며 각 기술마다 이런 Platform interface 가 더 존재한다.

--- src/main/java/tobyspring/tobyspringtwo/order/OrderService.java
+++ src/main/java/tobyspring/tobyspringtwo/order/OrderService.java
@@ -3,6 +3,7 @@ package tobyspring.tobyspringtwo.order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
@@ -12,7 +13,7 @@ import java.math.BigDecimal;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final JpaTransactionManager transactionManager;
+    private final PlatformTransactionManager transactionManager;
 
     public Order createOrder(final String no, final BigDecimal total) {
         final Order order = new Order(no, total);
