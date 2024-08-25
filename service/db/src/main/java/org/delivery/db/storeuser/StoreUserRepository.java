777e9caa5e0d400d482e6c0156c29c752b295b7f
jihwan <jihwan1535@nate.com>
2024-08-26 08:59:32 +0900

    feat: store user base entity

---
+++ service/db/src/main/java/org/delivery/db/storeuser/StoreUserRepository.java
@@ -0,0 +1,11 @@
+package org.delivery.db.storeuser;
+
+import java.util.Optional;
+import org.delivery.db.storeuser.vo.StoreUserStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreUserRepository extends JpaRepository<StoreUser, Long> {
+
+    Optional<StoreUser> findFirstByEmailAndStatusOrderByIdDesc(String email, StoreUserStatus status);
+
+}
