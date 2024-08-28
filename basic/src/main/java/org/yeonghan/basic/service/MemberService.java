32e4ab16f6233ce0bd1a8fbb6c7d35b6511cfae4
jihwan <jihwan1535@nate.com>
2024-08-28 17:30:59 +0900

    feat: member service 작성 및 테스트

---
+++ basic/src/main/java/org/yeonghan/basic/service/MemberService.java
@@ -0,0 +1,37 @@
+package org.yeonghan.basic.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.yeonghan.basic.domain.Member;
+import org.yeonghan.basic.repository.MemberRepository;
+import org.yeonghan.basic.repository.MemoryMemberRepository;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findMember(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(it -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+}
