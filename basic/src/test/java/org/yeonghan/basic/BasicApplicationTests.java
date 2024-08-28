6d70b74db728da20e8624e181b2d6083cf1424fa
jihwan <jihwan1535@nate.com>
2024-08-28 14:43:13 +0900

    chore: 김영한 강의 init

---
+++ basic/src/test/java/org/yeonghan/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yeonghan.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
