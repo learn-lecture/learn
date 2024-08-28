62192dea1ca5b093920dd4292574cba9a2ec48fa
jihwan <jihwan1535@nate.com>
2024-08-28 19:22:04 +0900

    refactor: jdbc template repository

--- basic/src/main/java/org/yeonghan/basic/SpringConfig.java
+++ basic/src/main/java/org/yeonghan/basic/SpringConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.yeonghan.basic.repository.JdbcMemberRepository;
+import org.yeonghan.basic.repository.JdbcTemplateMemberRepository;
 import org.yeonghan.basic.repository.MemberRepository;
 import org.yeonghan.basic.repository.MemoryMemberRepository;
 import org.yeonghan.basic.service.MemberService;
@@ -18,7 +19,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(DataSource dataSource) {
-        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
