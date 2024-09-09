5cb8130b9b03570ad8f6ce828e08b9ba5cafdf13
jihwan <jihwan1535@nate.com>
2024-09-09 13:07:29 +0900

    feat: query dsl 추가

---
+++ src/main/java/org/study/common/config/QueryDslConfig.java
@@ -0,0 +1,20 @@
+package org.study.common.config;
+
+import ch.qos.logback.classic.spi.ConfiguratorRank;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+
+@ConfiguratorRank
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
