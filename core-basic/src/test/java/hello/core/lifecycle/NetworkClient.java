37c359738d6654528dad107bbdce814f0cf5689a
jihwan <jihwan1535@nate.com>
2024-09-10 20:39:20 +0900

    refactor: bean annotation으로 init, destroy 처리

--- core-basic/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core-basic/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -27,15 +27,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("disconnect = " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         System.out.print("NetworkClient afterPropertiesSet ==>  ");
         connect();
         call("초기화 연결");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         System.out.print("NetworkClient destroy =>  ");
         disconnect();
     }
