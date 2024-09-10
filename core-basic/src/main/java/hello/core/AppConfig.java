70339d3a3fa1224b973f2c795e57c2f32d10cded
jihwan <jihwan1535@nate.com>
2024-09-10 15:55:58 +0900

    fix: bean 충돌 해결

--- core-basic/src/main/java/hello/core/AppConfig.java
+++ core-basic/src/main/java/hello/core/AppConfig.java
@@ -10,27 +10,36 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-//@Configuration
+@Configuration
 public class AppConfig {
 
+    @Primary
     @Bean
     public MemberService memberService() {
+        System.out.println("memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Primary
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("memberRepository");
         return new MemoryMemberRepository();
     }
 
+    @Primary
     @Bean
     public OrderService orderService() {
+        System.out.println("orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Primary
     @Bean
     public DiscountPolicy discountPolicy() {
+        System.out.println("discountPolicy");
         return new FixDiscountPolicy();
     }
 
