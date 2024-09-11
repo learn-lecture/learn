07663ffce9b930ad01113aeb486b2605b0fa3404
jihwan <jihwan1535@nate.com>
2024-09-11 17:49:39 +0900

    feat: request scope bean과 provider로 logger 작성

---
+++ core-basic/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,32 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import java.util.UUID;
+import lombok.Setter;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("request")
+public class MyLogger {
+
+    private String uuid;
+    @Setter
+    private String requestUrl;
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "] " + "[" + requestUrl + "] " + message);
+    }
+
+    @PostConstruct
+    private void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create: " + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close: " + this);
+    }
+}
---
+++ core-basic/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,31 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        String requestUrl = request.getRequestURL().toString();
+        myLogger.setRequestUrl(requestUrl);
+
+        myLogger.log("controller test");
+        Thread.sleep(1000);
+        logDemoService.logic("testId");
+        return "OK";
+    }
+
+}
