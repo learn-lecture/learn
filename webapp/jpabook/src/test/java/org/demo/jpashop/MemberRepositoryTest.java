a1205bb63561a0857288fc0809c93b2073959701
jihwan <jihwan1535@nate.com>
2025-01-06 16:50:51 +0900

    feat: jpa 설정 추가

---
+++ webapp/jpabook/src/test/java/org/demo/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package org.demo.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    void testMember() {
+        // given
+        Member member = new Member();
+        member.setUserName("memberA");
+        Long savedId = memberRepository.save(member);
+
+        // when
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+    }
+
+}
