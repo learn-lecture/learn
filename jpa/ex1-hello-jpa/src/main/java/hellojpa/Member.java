8ef7ba0f8351678eccc80834b5f4e131d10a925c
jihwan <jihwan1535@nate.com>
2025-01-06 15:26:57 +0900

    learn: hibernate jpa crud 학습

---
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,36 @@
+package hellojpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Id
+    private Long id;
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
