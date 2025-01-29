5f45cebae9dcc59ee6ae6f508d76b8836dece79a
jihwan <jihwan1535@nate.com>
2025-01-29 11:06:12 +0900

    feat: 주문 조회(컬렉션) v5 작성

--- webapp/jpabook2/src/main/java/org/demo/jpashop/repository/order/query/OrderQueryRepository.java
+++ webapp/jpabook2/src/main/java/org/demo/jpashop/repository/order/query/OrderQueryRepository.java
@@ -2,6 +2,8 @@ package org.demo.jpashop.repository.order.query;
 
 import jakarta.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.demo.jpashop.domain.Order;
 import org.springframework.stereotype.Repository;
@@ -40,6 +42,33 @@ public class OrderQueryRepository {
                         + " join o.member m"
                         + " join o.delivery d", OrderQueryDto.class
         ).getResultList();
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> orders = findOrders();
+
+        List<Long> orderIds = orders.stream().map(OrderQueryDto::getOrderId).toList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemByOrderId = findOrderItemByOrderId(orderIds);
+
+        orders.forEach(o -> o.setOrderItems(orderItemByOrderId.get(o.getOrderId())));
+
+        return orders;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemByOrderId(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                        "select new org.demo.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)"
+                                + " from OrderItem oi"
+                                + " join oi.item i"
+                                + " where oi.order.id in :orderIds", OrderItemQueryDto.class
+                )
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemByOrderId = orderItems.stream()
+                .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+        return orderItemByOrderId;
     }
 
 }
