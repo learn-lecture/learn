337dfd8c15ee4270dc9843838244421dd6618404
jihwankim128 <jihwan1535@nate.com>
2025-03-28 15:18:40 +0900

    Keep only service directory content and rename to delivery

--- delivery/chat-service/src/main/java/org/demo/chatservice/chat/repository/JpaChatroomRepository.java
+++
@@ -1,7 +0,0 @@
-package org.demo.chatservice.chat.repository;
-
-import org.demo.chatservice.chat.repository.entities.Chatroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaChatroomRepository extends JpaRepository<Chatroom, Long> {
-}
--- delivery/chat-service/src/main/java/org/demo/chatservice/member/repository/enums/Gender.java
+++
@@ -1,5 +0,0 @@
-package org.demo.chatservice.member.repository.enums;
-
-public enum Gender {
-    MALE, FEMALE
-}
